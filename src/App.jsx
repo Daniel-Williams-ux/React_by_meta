7eacfc5256047c1b21897d82903c52382ad92106
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-20 06:16:37 +0000

    Inline styling and attributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Heading from './components/Heading';
+import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css'
@@ -6,6 +7,7 @@ import './App.css'
 export default function App() {
   return (
     <div>
+     <Nav />
      <Heading greet="Welcome" />
      <Main content="Welcome to the body section" /> 
      <Footer greet="Goodbye" />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+function Nav() {
+ const navLinks = {
+  background: "gold",
+ }
+  return (
+   <>
+    <nav style={navLinks}> 
+      <ul>
+       <li>Home</li>
+       <li>About</li>  
+       <li>List</li>
+     </ul>
+    </nav>
+   </>
+  )
+}
+
+export default Nav;
