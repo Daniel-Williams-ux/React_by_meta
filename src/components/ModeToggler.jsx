4d5ab25414eed330a73327e4b16f128c6e90f2eb
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-21 13:54:15 +0000

    More on dynamic events

--- src/components/ModeToggler.jsx
+++ src/components/ModeToggler.jsx
@@ -1,14 +1,25 @@
-function Btn(props) {
- const darkMode = true;
- const darkModeOn = <h3>Dark Mode is On</h3>;
- const lightModeOn = <h3>Light Mode is On</h3>;
+function ModeToggler() {
+  let darkModeOn = true;
+  const darkMode = <h3>Dark Mode is On</h3>;
+  const lightMode = <h3>Light Mode is On</h3>;
 
-
- return (
-  <div> 
-    {darkMode ? darkModeOn : lightModeOn}
-  </div>
- );
+  function handleClick() {
+    darkModeOn = !darkModeOn;
+    if (darkModeOn === true) {
+      console.log("Dark Mode is On")
+    } else {
+      console.log("Light Mode is On")
+    }
+  }
+  
+  return (
+    <div> 
+      {darkModeOn ? darkMode : lightMode}
+      <button onClick={handleClick} >
+        Click Me!
+      </button>
+    </div>
+  );
 };
 
-export default Btn;
+export default ModeToggler;
