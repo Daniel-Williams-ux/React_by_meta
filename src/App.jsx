cb4df23489c62cd409aba66e32f191b3c7ff45f4
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-25 15:22:16 +0000

    state change basics in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import Heading from './components/Heading';
 import './App.css'
 
 export default function App() {
-  const [word, setWord] = React.useState("hello")
+  const [word, setWord] = React.useState("Eat")
+  function handleClick() {
+     setWord("Drink")
+  }
   return (
-    <div>
-      <h2>State value: {word}</h2>
+    <div className="App">
+     <Heading message={word + " at Little Lemon"} />
+      <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
