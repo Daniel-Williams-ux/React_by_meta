95d52f60645a3ceb6666c38861af6cc40140f601
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-25 10:36:48 +0000

    statefullnes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import React, {useState} from 'react'
 import './App.css'
 
 export default function App() {
+  const [word, setWord] = React.useState("hello")
   return (
     <div>
-      <h2>I am a stateless component</h2>
+      <h2>State value: {word}</h2>
     </div>
   )
 }
