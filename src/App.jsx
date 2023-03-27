fe70868768e8878c0b1b63e9922991517b04e69a
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-27 10:47:03 +0000

    context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
-import Heading from './components/Heading';
+import MealsProvider from './providers/MealsProvider';
+import MealsList from './components/MealsList';
+import Counter from './components/Counter';
 import './App.css'
 
 export default function App() {
-  const [word, setWord] = React.useState("Eat")
-  function handleClick() {
-     setWord("Drink")
-  }
   return (
-    <div className="App">
-     <Heading message={word + " at Little Lemon"} />
-      <button onClick={handleClick}>Click me</button>
+    <div>
+     <MealsProvider />
+       <MealsList />
+       <Counter />
+     <MealsProvider/> 
     </div>
   )
 }
--- src/components/Heading.jsx
+++
@@ -1,7 +0,0 @@
-function Heading(props) {
-  return (
-    <h1>{props.message}</h1>
-  );
-};
-
-export default Heading;
