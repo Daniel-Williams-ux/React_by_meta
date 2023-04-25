b76aa07cdb73a6e5858b700be79aa7b70dfa66a0
Daniel Williams <williamsoligodaniel@gmail.com>
2023-04-25 15:57:41 +0100

    Update Creating_and_nesting_components .jsx

--- Quick_start/Creating_and_nesting_components .jsx
+++ Quick_start/Creating_and_nesting_components .jsx
@@ -1,1 +1,20 @@
-const
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+/*Notice that <MyButton /> starts with a capital letter. That’s how you know it’s a React component. 
+React component names must always start with a capital letter, while HTML tags must be lowercase.
+*/
