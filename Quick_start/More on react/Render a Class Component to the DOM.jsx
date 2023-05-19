d6f7cc692123d49eba522272fdfcef20dd96409b
Daniel Williams <williamsoligodaniel@gmail.com>
2023-05-19 10:39:24 -0700

    Create Render a Class Component to the DOM.jsx

---
+++ Quick_start/More on react/Render a Class Component to the DOM.jsx
@@ -0,0 +1,38 @@
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        <Vegetables />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+
+// Change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+
+/*
+Types of Food:
+Fruits:
+Non-Citrus:
+Apples
+Blueberries
+Strawberries
+Bananas
+Citrus:
+Lemon
+Lime
+Orange
+Grapefruit
+Vegetables:
+Brussel Sprouts
+Broccoli
+Squash
+*/
