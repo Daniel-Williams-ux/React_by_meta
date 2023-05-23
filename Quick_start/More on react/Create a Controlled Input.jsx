4e172d2b62c3ac0355a92af5788837170c109d52
Daniel Williams <williamsoligodaniel@gmail.com>
2023-05-23 17:21:19 +0100

    Update Create a Controlled Input.jsx

--- Quick_start/More on react/Create a Controlled Input.jsx
+++ Quick_start/More on react/Create a Controlled Input.jsx
@@ -26,3 +26,24 @@ class ControlledInput extends React.Component {
     );
   }
 };
+
+//or
+import React, { useState } from 'react';
+
+const ControlledInput = () => {
+  const [input, setInput] = useState('');
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <input value={input} onChange={handleChange} />
+      <h4>Controlled Input:</h4>
+      <p>{input}</p>
+    </div>
+  );
+};
+
+export default ControlledInput;
